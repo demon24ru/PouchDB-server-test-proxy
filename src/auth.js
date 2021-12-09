2d2afae0b7003ee126a3ce461660b7b2e81d9c04
demon-dem <ittek@inbox.ru>
2021-12-09 13:58:57 +0700

    init

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,47 @@
 const jwt = require('jsonwebtoken');
-const utils = require("express-pouchdb/lib/utils");
 
 const accessTokenSecret = '6s54dfytrtrtr4s65d4fs6fd46s5fd45s64df4';
 const refreshTokenSecret = '6s54dfytrtrtr4s65d4fs6fd46s5fd45s64df4';
 const accessTokenExp = 2592000; // 30 days
+
+function setJsonOrPlaintext(res) {
+    // Send the client application/json if they asked for it,
+    // else send text/plain; charset=utf-8. This mimics CouchDB.
+    const type = res.req.accepts(['text', 'json']);
+    if (type === "json") {
+        res.setHeader('Content-Type', 'application/json');
+    } else {
+        //adds ; charset=utf-8
+        res.type('text/plain');
+    }
+}
+
+function sendJSON(res, status, body) {
+    res.status(status);
+    setJsonOrPlaintext(res);
+    res.send(jsonToBuffer(body));
+}
+
+function sendError(res, err, baseStatus) {
+    const status = err.status || baseStatus || 500;
+
+    // last argument is optional
+    if (err.name && err.message) {
+        if (err.name === 'Error' || err.name === 'TypeError') {
+            if (err.message.indexOf("Bad special document member") !== -1) {
+                err.name = 'doc_validation';
+                // add more clauses here if the error name is too general
+            } else {
+                err.name = 'bad_request';
+            }
+        }
+        err = {
+            error: err.name,
+            reason: err.message
+        };
+    }
+    sendJSON(res, status, err);
+}
 
 function generateJwtTokens(id) {
     const access = jwt.sign(
@@ -51,9 +89,9 @@ function refreshToken(req, res) {
 
 function auth(req, res, next){
     // ignore requests without authorization
-    const auth = req.get("authorization");
+    const auth = req.headers.authorization;
     if (!auth || !/^Bearer /.test(auth))
-        return utils.sendError(res, new Error('Unauthorized'), 401);
+        return sendError(res, new Error('Unauthorized'), 401);
 
     const token = auth.substr(6).trim();
     let payload;
@@ -61,7 +99,7 @@ function auth(req, res, next){
     try {
         payload = verify(token);
     } catch(e) {
-        return utils.sendError(res, e, 401);
+        return sendError(res, e, 401);
     }
 
     if (typeof payload.id !== "string") payload.id = null;
@@ -75,4 +113,5 @@ module.exports = {
     generateJwtTokens,
     refreshToken,
     auth,
+    sendError,
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const { generateJwtTokens, auth, refreshToken } = require('./auth');
+const { generateJwtTokens, auth, refreshToken, sendError } = require('./auth');
 const jwt = require('jsonwebtoken');
 const qs = require('qs');
 const httpProxy = require('http-proxy');
-const apiProxy = httpProxy.createProxyServer();
 
+const apiProxy = httpProxy.createProxyServer({
+    target: 'http://192.168.8.1:5984'
+});
 
 const express = require('express');
 const app = express();
@@ -15,7 +17,7 @@ app.use('/db', auth, function(req, res) {
     const sep = url.split('/');
 
     if (sep[1] && sep[1] !== 'queue')
-        throw new Error('Forbidden');
+        sendError(res, new Error('Forbidden'), 403);
 
     const filter = {
         filter: 'myfilter/myfilter',
@@ -23,10 +25,12 @@ app.use('/db', auth, function(req, res) {
     };
 
     if (/_changes\?/.test(req.url)) {
+        console.log('url=>', url);
         req.url = `${req.url}&${qs.stringify(filter)}`;
+        console.log('req.url=> %j', req.url);
         // Object.assign(req.query, filter);
     }
-    apiProxy.web(req, res, { target: 'http://192.168.8.1:5984' });
+    apiProxy.web(req, res);
 });
 
 app.use(express.json());
@@ -39,15 +43,15 @@ app.post('/refresh', (req, res, next)=> {
     return refreshToken(req, res);
 });
 
-app.post('/decode', (req, res, next)=> {
-    const { token } = req.body;
-    res.status(200).json(
-        jwt.decode(token, {
-            algorithms: ["HS256"]
-        })
-    );
-});
+// app.post('/decode', (req, res, next)=> {
+//     const { token } = req.body;
+//     res.status(200).json(
+//         jwt.decode(token, {
+//             algorithms: ["HS256"]
+//         })
+//     );
+// });
 
-app.listen(4000, ()=>{
+app.listen(5984, ()=>{
     console.log('Listen on port 4000');
 })
